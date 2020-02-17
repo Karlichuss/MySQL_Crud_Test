69c7b458c68b5715303e4b1809e453d4cb383e0b
AntonioCarlosOrdonez_MainHomePC <carlossant791@hotmail.com>
2020-02-17 15:05:57 +0100

    Conectado a base de datos localhost y controlador operativo en Index

---
+++ WebApplication1/WebApplication1/Controllers/contactosController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class contactosController : Controller
+    {
+        private test_crudEntities db = new test_crudEntities();
+
+        // GET: contactos
+        public async Task<ActionResult> Index()
+        {
+            return View(await db.contactos.ToListAsync());
+        }
+
+        // GET: contactos/Details/5
+        public async Task<ActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contactos contactos = await db.contactos.FindAsync(id);
+            if (contactos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactos);
+        }
+
+        // GET: contactos/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: contactos/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "id,nombre,email,fecha_creacion")] contactos contactos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.contactos.Add(contactos);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View(contactos);
+        }
+
+        // GET: contactos/Edit/5
+        public async Task<ActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contactos contactos = await db.contactos.FindAsync(id);
+            if (contactos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactos);
+        }
+
+        // POST: contactos/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que desea enlazarse. Para obtener 
+        // más información vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "id,nombre,email,fecha_creacion")] contactos contactos)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contactos).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(contactos);
+        }
+
+        // GET: contactos/Delete/5
+        public async Task<ActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            contactos contactos = await db.contactos.FindAsync(id);
+            if (contactos == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contactos);
+        }
+
+        // POST: contactos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+            contactos contactos = await db.contactos.FindAsync(id);
+            db.contactos.Remove(contactos);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
